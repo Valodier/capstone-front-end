26ac05078bdf9b066e7c991618681c2ba3940783
William Meyer <valodier@icloud.com>
2022-04-29 14:07:48 -0400

    Adds view/router/link for rooms index get

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RoomsIndex from "../views/RoomsIndex.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/rooms",
+    name: "rooms",
+    component: RoomsIndex,
   },
   {
     path: "/about",
--- src/views/RoomsIndex.vue
+++ src/views/RoomsIndex.vue
@@ -1,3 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Rooms!",
+      rooms: [],
+    };
+  },
+  created: function () {
+    this.roomsIndex();
+  },
+  methods: {
+    roomsIndex: function () {
+      axios.get("/rooms.json").then((response) => {
+        this.rooms = response.data;
+        console.log("Rooms Index Retrieves", response.data);
+      });
+    },
+  },
+};
+</script>
